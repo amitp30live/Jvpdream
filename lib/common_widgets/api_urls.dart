e8c6f5f627f5088f94090f18d4f5ca6fe8d5cb45
AmitP <amitp30.live@gmail.com>
2023-01-02 13:01:03 +0530

    Added Signup API call

--- lib/common_widgets/api_urls.dart
+++
@@ -1,5 +0,0 @@
-class ApiURLS {
-  static const baseUrl = "http://localhost:4000/";
-  static const signupURL = "${baseUrl}auth/register";
-  static const loginURL = "${baseUrl}auth/login";
-}
---
+++ lib/resources/api_urls.dart
@@ -0,0 +1,5 @@
+class ApiURLS {
+  static const baseUrl = "http://localhost:4000/";
+  static const signupURL = "${baseUrl}auth/register";
+  static const loginURL = "${baseUrl}auth/login";
+}
